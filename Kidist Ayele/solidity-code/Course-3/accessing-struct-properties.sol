feda003122d12bde3fda523ae7ab57fa31d8d936
Kidist-Ayele <kidistayele37@gmail.com>
2026-05-17 23:53:50 +0300

    fix error

---
+++ Kidist Ayele/solidity-code/Course-3/accessing-struct-properties.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+	enum Choices { Yes, No }
+	
+	struct Vote {
+		Choices choice;
+		address voter;
+	}
+	
+	// TODO: create a public state variable: an array of votes
+	Vote[] public votes;
+
+	function createVote(Choices choice) external {
+		// TODO: add a new vote to the array of votes state variable
+		votes.push(Vote(choice, msg.sender));
+	}
+
+	function hasVoted(address _voter) external view returns (bool) {
+		for (uint i = 0; i < votes.length; i++) {
+			if (votes[i].voter == _voter) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function findChoice(address _voter) external view returns (Choices) {
+		for (uint i = 0; i < votes.length; i++) {
+			if (votes[i].voter == _voter) {
+				return votes[i].choice;
+			}
+		}
+		return Choices.Yes;
+	}
+}
