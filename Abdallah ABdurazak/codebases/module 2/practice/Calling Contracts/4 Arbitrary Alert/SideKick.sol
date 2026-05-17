0ea2d07438e45f9b4deb9291ec5a3b897d736b64
Abdallah Abdurazak <134734720+Abd453@users.noreply.github.com>
2026-05-17 21:05:03 +0300

    Delete Abdallah ABdurazak directory

--- Abdallah ABdurazak/codebases/module 2/practice/Calling Contracts/4 Arbitrary Alert/SideKick.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Sidekick {
-    function relay(address hero, bytes calldata data) external {
-        (bool success, ) = hero.call(data);
-        require(success);
-    }
-}
--- Abdallah ABdurazak/codebases/module 2/practice/Calling Contracts/5 Fallback/SideKick.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Sidekick {
-    function makeContact(address hero) external {
-           (bool success, ) = hero.call("0x12345678");
-        require(success);
-    }
-}
